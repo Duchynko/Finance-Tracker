2f116a540dab8bbf3395c76bac3256347e8ec4ff
Jakub Duchon <jakub.duchon9@gmail.com>
2018-02-20 18:20:21 +0000

    Add user and stocks association plus views

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,13 @@
 class StocksController < ApplicationController
   
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookop(params[:stock])
-      if @stock
-        render partial: 'users/result'
-      else
-        flash.now[:danger] = "You have entered an incoret symbol"
-        render partial: 'users/result'
-      end
-    else
+    if params[:stock].blank?
       flash.now[:danger] = "You have entered an empty search string"
-        render partial: 'users/result'
+    else
+      @stock = Stock.new_from_lookop(params[:stock])
+      flash.now[:danger] = "You have entered an incoret symbol" unless @stock
     end
+    render partial: 'users/result'
   end
   
 end
